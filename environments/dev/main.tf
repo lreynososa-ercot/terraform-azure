b75a9b237cf4a71aec6c5653f25168370801719c
Luis Antonio Reynoso <telemaco_81@hotmail.com>
2025-03-09 23:18:53 -0500

    Refactor OIDC configuration formatting in Terraform provider blocks

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -40,9 +40,9 @@ terraform {
 
 provider "azurerm" {
   features {}
-  
+
   # OIDC Configuration
-  use_oidc = true
+  use_oidc        = true
   subscription_id = var.subscription_id
   tenant_id       = var.tenant_id
 }
